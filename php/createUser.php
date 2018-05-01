9bc7bdc72aa1382cb90d566a672894b8786359de
Jamie Thomas <31860384+JamieThomas-WKU@users.noreply.github.com>
2018-04-30 20:16:10 -0500

    Add files via upload

---
+++ php/createUser.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  CS 270 - Group Project
+  ARK (Animal Record Keeper)
+  Trevor D. Brown, Jamie Thomas, Colin Whitesel, Brian Bulka
+
+  createUser.php - creates a new user entry for ARK.
+*/
+
+require("./common.php");
+
+$user = $_POST["user"];
+$password = $_POST["password"];
+
+$NewUserIntoDatabaseSQL = "INSERT INTO users (user, hpass) VALUES (" . $user . "," . $password . ")";
+
+$response = insertUpdateDatabase($NewUserIntoDatabaseSQL);
+SendJSON($response);
+?>
---
+++ php/top.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>ARK (Animal Record Keeper)</title>
+
+		<meta name="description" content="A.R.K">
+		<meta name="Colin Whitesel" content="WKU 270">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+		<link rel="stylesheet" href="./css/style.css">
+		<link href="./images/arkFavicon.jpg" type="image/gif" rel="shortcut icon" />
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="./js/scripts.js"> </script>
+   	</head>
+   	<body>
+		<div class="navigation-bar">
+			<div class="navigation-bar-title">
+				A.R.K.
+			</div>
+			<div class="navigation-bar-links">
+				<a class="button navigation-link" href="index.php">Home</a>
+				<a class="button navigation-link" href="logout.php">Sign Out</a>
+			</div>
+		</div>
